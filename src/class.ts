3402c261016bd8c62856d0232b30e140c7f6ad2d
bnpysse <bnpysse@aliyun.com>
2024-06-30 16:54:46 +0800

    类属性

--- src/class.ts
+++ src/class.ts
@@ -30,3 +30,26 @@ class Student extends Person {
 }
 let s: Student = new Student('Jane', 30, 9);
 console.log(s.say())
+
+type Sex = 'Male' | 'Female'
+// 参数属性
+class Animal {
+    private _male: Sex
+    constructor(public name: string, public age: number) {}
+    set sex(val: Sex){
+        if (val !== 'Male' && val !== 'Female'){
+            throw new Error('人的性别只能是“Male”、“Female”')
+        }
+        this._male = val
+    }
+    get sex(): Sex {
+        return this._male
+    }
+}
+let a: Animal = new Animal('Lion', 2);
+console.log(a.name); // Lion
+let b: Animal = new Animal('Tiger', 3);
+b.sex = 'Male'; // 正确
+console.log(b.name,' and ', b.sex); // Tiger
+a.sex = 'Female'; // 正确
+console.log(a.name,' and ', a.sex); // Tiger
