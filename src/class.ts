c83175d2972556872a6d5aea26807a3bfc558266
bnpysse <bnpysse@aliyun.com>
2024-06-30 14:52:50 +0800

    learning extends of class

--- src/class.ts
+++ src/class.ts
@@ -9,5 +9,24 @@ class Person {
     say(msg: string='Hello,'):string {
         return `${msg} ${this.name}! I am ${this.age} years old.`;
     }
+    static food: string = 'Pizza';
+    static eat() {
+        console.log(`I like ${Person.food}`);
+    }
 }
+let p: Person = new Person('John', 25);
+console.log(p.say())
+Person.eat();
 
+class Student extends Person {
+    grade: number;
+    constructor(name:string, age:number, grade:number) {
+        super(name, age);
+        this.grade = grade;
+    }
+    say():string {
+        return `${super.say()} I am in grade ${this.grade}.`;
+    }
+}
+let s: Student = new Student('Jane', 30, 9);
+console.log(s.say())
