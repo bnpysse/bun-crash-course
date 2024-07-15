b55cdf50ece2b502cb8c2f17d989048d7238254a
bnpysse <bnpysse@aliyun.com>
2024-07-16 02:22:59 +0800

    04.Namespace,error...

--- src/mathUtils.ts
+++ src/mathUtils.ts
@@ -1,3 +1,11 @@
-export function add(a: number, b: number): number {
-    return a + b;
+// export function add(a: number, b: number): number {
+//     return a + b;
+// }
+namespace Geometry {
+    export function area(width: number, height: number): number {
+        return width * height;
+    }
+    export function perimeter(width: number, height: number): number {
+        return 2 * (width + height);
+    }
 }
--- src/module.ts
+++ src/module.ts
@@ -1,2 +1,6 @@
-import {add} from './mathUtils';
-console.log(add(8, 2));
+// import {add} from './mathUtils';
+// import {area, perimeter} from './mathUtils';
+// console.log(add(8, 2));
+import Geometry from './mathUtils';
+console.log(Geometry.area(10, 20));
+console.log(Geometry.perimeter(10, 20));
